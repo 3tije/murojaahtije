229e582348f9c851b4f684428e1cd72f00c3359f
3tije <b177tri@gmail.com>
2026-06-21 14:12:55 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,21 @@
-const CACHE_NAME = 'murojaah-tije-v1.24';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './service-worker.js',
-  './data/quran-sample.json'
-];
+const CACHE_NAME = 'murojaah-tije-v1';
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ])
+    )
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
-      const clone = resp.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-      return resp;
-    }).catch(() => caches.match('./index.html')))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
